0000ec33d46293ed0df773f507f7cf5ebbe72c23
Matt Hooper <mattt.hoooper@gmail.com>
2021-04-10 10:50:11 +0100

    Use format strings and inferred actor
    123720

--- git_mine.py
+++ git_mine.py
@@ -5,12 +5,15 @@ import time
 from datetime import datetime
 
 repo = git.Repo('.')
-
 c = repo.head.commit
 
+print(c.author, c.author_tz_offset, c.author.conf_email)
+
+base = f'tree {c.tree.hexsha}'
+base += f'\nparent {c.parents[0].hexsha}'
+
 msg = repo.head.commit.message
-base = 'tree ' + c.tree.hexsha
-base += '\nparent ' + c.parents[0].hexsha
+actor = f'{c.author.name} <{c.author.email}>'
 
 dt = 5
 now = int(datetime.now().timestamp())
@@ -19,17 +22,17 @@ n = 0
 sha = ''
 new_msg = ''
 while not sha.startswith('0000'):
-    new_msg = msg + '\n' + str(n)
+    n += 1
+    new_msg = f'{msg}\n{str(n)}'
 
-    cat = base + '\nauthor Matt Hooper <mattt.hoooper@gmail.com> ' + str(int(c.authored_datetime.timestamp())) + ' +0100'
-    cat += '\ncommitter Matt Hooper <mattt.hoooper@gmail.com> ' + str(then) + ' +0100'
-    cat += '\n\n' + new_msg + '\n'
+    cat = base + f'\nauthor {actor} {str(int(c.authored_datetime.timestamp()))} +0100'
+    cat += f'\ncommitter {actor} {str(then)} +0100'
+    cat += f'\n\n{new_msg}\n'
 
     bs = len(cat.encode('utf-8'))
-    ss = 'commit ' + str(bs) + '\0' + cat
+    ss = f'commit {str(bs)}\0{cat}'
 
     sha = hashlib.sha1(ss.encode('utf-8')).hexdigest()
-    n += 1
     if n % 1000 == 0:
         print(n)
 
